88cc806b0c598954bd2c0e2bdd3243dd11502afe
cedogu <86116050+cedogu@users.noreply.github.com>
2022-09-24 13:36:51 +0300

    Prime Number Calculation

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-       int number= 26905;
+       int number= 1;
        int remainder = number % 2;
        boolean isPrime= true;
+
+       if(number==1){
+           System.out.println("Value 1 is not a prime number");
+           return;
+       }
+
+       if(number<2){
+           System.out.println("Unvalid value");
+       }
 
        for (int i=2; i<number; i++){
            if(number%i==0){
